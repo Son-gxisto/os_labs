38822c85e8962d659bdb47a7004e76a479201e6e
root <nikitoc17.09@gmail.com>
2020-10-05 09:21:09 +0300

    2

--- lab2/2_pid.sh
+++ lab2/2_pid.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -afx | grep -E ":[[:digit:]]{2} [[:alpha:]\/[:digit:]]*\/sbin\/" | awk '{print($1)}' > "$PWD/2.f"
+ps -afx | grep -E ":[[:digit:]]{2} [[:alpha:]\/[:digit:]]*usr\/sbin\/" | awk '{print($1)}' > "$PWD/2.f"
